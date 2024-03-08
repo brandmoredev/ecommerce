2cd0c41df99e027c89aaa26379f99fc271994c2d
brandmoredev <brandmoredev@gmail.com>
2024-03-08 11:15:08 +0300

    Updates filters

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,12 +1,38 @@
+"use client"
+
 import React from 'react'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
 
-const Filters = () => {
+const Filters = ({ categories }: { categories: Category[]}) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {
+
+  }
+
   return (
     <div className={classes.filters}>
       <div>
         <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map((category) => {
+            const isSelected = false;
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div> 
       </div>
     </div>
   )
